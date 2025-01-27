066aed1a58bb381eee2295f77e1b001f0f708afa
Muzzamil K <muzzamilahmad@ymail.com>
2025-01-27 12:42:03 -0500

    Adding house

---
+++ 16-haunted-house/src/script.js
@@ -0,0 +1,293 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { Timer } from 'three/addons/misc/Timer.js'
+import GUI from 'lil-gui'
+
+/**
+ * Base
+ */
+// Debug
+const gui = new GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Textures * 
+ */
+const textureLoader = new THREE.TextureLoader()
+
+// Floor
+const floorAlphaTexture = textureLoader.load('./floor/alpha.jpg')
+const floorColorTexture = textureLoader.load('./floor/coast_sand_rocks_02_1k/coast_sand_rocks_02_diff_1k.jpg')
+const floorARMTexture = textureLoader.load('./floor/coast_sand_rocks_02_1k/coast_sand_rocks_02_arm_1k.jpg')
+const floorNormalTexture = textureLoader.load('./floor/coast_sand_rocks_02_1k/coast_sand_rocks_02_nor_gl_1k.jpg')
+const floorDisplacementTexture = textureLoader.load('./floor/coast_sand_rocks_02_1k/coast_sand_rocks_02_disp_1k.jpg')
+floorColorTexture.colorSpace = THREE.SRGBColorSpace
+
+floorColorTexture.repeat.set(8,8)
+floorColorTexture.wrapS = THREE.RepeatWrapping
+floorARMTexture.wrapS = THREE.RepeatWrapping
+floorNormalTexture.wrapS = THREE.RepeatWrapping
+floorDisplacementTexture.wrapS = THREE.RepeatWrapping
+
+floorColorTexture.wrapT = THREE.RepeatWrapping
+floorARMTexture.wrapT = THREE.RepeatWrapping
+floorNormalTexture.wrapT = THREE.RepeatWrapping
+floorDisplacementTexture.wrapT = THREE.RepeatWrapping
+
+// Wall
+const wallColorTexture = textureLoader.load('./wall/castle_brick_broken_06_1k/castle_brick_broken_06_diff_1k.jpg')
+const wallARMTexture = textureLoader.load('./wall/castle_brick_broken_06_1k/castle_brick_broken_06_arm_1k.jpg')
+const wallNormalTexture = textureLoader.load('./wall/castle_brick_broken_06_1k/castle_brick_broken_06_nor_gl_1k.jpg')
+wallColorTexture.colorSpace = THREE.SRGBColorSpace
+
+// Roof
+const roofColorTexture = textureLoader.load('./roof/roof_slates_02_1k/roof_slates_02_diff_1k.jpg')
+const roofARMTexture = textureLoader.load('./roof/roof_slates_02_1k/roof_slates_02_arm_1k.jpg')
+const roofNormalTexture = textureLoader.load('./roof/roof_slates_02_1k/roof_slates_02_nor_gl_1k.jpg')
+
+roofColorTexture.colorSpace = THREE.SRGBColorSpace
+
+roofColorTexture.repeat.set(3,1)
+roofARMTexture.repeat.set(3,1)
+roofNormalTexture.repeat.set(3,1)
+
+roofColorTexture.wrapS = THREE.RepeatWrapping
+roofARMTexture.wrapS = THREE.RepeatWrapping
+roofNormalTexture.wrapS = THREE.RepeatWrapping
+
+// Bush
+const bushColorTexture = textureLoader.load('./bush/leaves_forest_ground_1k/leaves_forest_ground_diff_1k.jpg')
+const bushARMTexture = textureLoader.load('./bush/leaves_forest_ground_1k/leaves_forest_ground_arm_1k.jpg')
+const bushNormalTexture = textureLoader.load('./bush/leaves_forest_ground_1k/leaves_forest_ground_nor_gl_1k.jpg')
+
+bushColorTexture.colorSpace = THREE.SRGBColorSpace
+
+bushColorTexture.repeat.set(2, 1)
+bushARMTexture.repeat.set(2, 1)
+bushNormalTexture.repeat.set(2, 1)
+
+bushColorTexture.wrapS = THREE.RepeatWrapping
+bushARMTexture.wrapS = THREE.RepeatWrapping
+bushNormalTexture.wrapS = THREE.RepeatWrapping
+/**
+ * House
+ */
+// Temporary sphere
+// const sphere = new THREE.Mesh(
+//     new THREE.SphereGeometry(1, 32, 32),
+//     new THREE.MeshStandardMaterial({ roughness: 0.7 })
+// )
+// scene.add(sphere)
+const floorMaterial =     new THREE.MeshStandardMaterial({
+    alphaMap: floorAlphaTexture,
+    transparent: true,
+    map: floorColorTexture,
+    aoMap: floorARMTexture,
+    roughnessMap: floorARMTexture,
+    metalnessMap: floorARMTexture,
+    normalMap: floorNormalTexture,
+    displacementMap: floorDisplacementTexture,
+    displacementScale: 0.32,
+    displacementBias: -0.1
+
+}) //for a realistic look
+// floor
+
+const floor = new THREE.Mesh(
+    new THREE.PlaneGeometry(20,20, 100, 100), // 
+    floorMaterial
+)
+floor.rotation.x = -Math.PI * 0.5
+// floor.position.y = -1.25
+scene.add(floor) 
+
+gui.add(floor.material, 'displacementScale').min(0).max(1).step(0.001).name('floorDisplacementScale')
+gui.add(floor.material, 'displacementBias').min(-1).max(1).step(0.001).name('floorDisplacementBias')
+const house = new THREE.Group()
+scene.add(house)
+const houseMeasurements = {
+    x: 4,
+    y: 2.5,
+    z: 4
+}
+const walls = new THREE.Mesh(
+    new THREE.BoxGeometry(houseMeasurements.x, houseMeasurements.y, houseMeasurements.z),
+    new THREE.MeshStandardMaterial(
+        {
+            map: wallColorTexture,
+            aoMap: wallARMTexture,
+            roughnessMap: wallARMTexture,
+            metalnessMap: wallARMTexture,
+            normalMap: wallNormalTexture
+        }   
+    )
+)
+const roof = new THREE.Mesh(
+    new THREE.ConeGeometry(4, 3, 4),
+    new THREE.MeshStandardMaterial({
+        map: roofColorTexture,
+        aoMap: roofARMTexture,
+        roughnessMap: roofARMTexture,
+        metalnessMap: roofARMTexture,
+        normalMap: roofNormalTexture
+    })
+)
+roof.position.y = houseMeasurements.y + 1.5
+roof.rotation.y = Math.PI /2/2
+house.add(roof)
+house.add(walls) //  house is a part of the scene
+walls.position.y += houseMeasurements.y/2
+
+//Door
+const door = new THREE.Mesh(
+    new THREE.PlaneGeometry(2.2, 2.2),
+    new THREE.MeshStandardMaterial()
+)
+door.position.z = houseMeasurements.z/2 + .01
+door.position.y = 1
+house.add(door)
+
+//Bushes
+
+const bushGeometry = new THREE.SphereGeometry(1, 16, 16)
+const bushMaterial = new THREE.MeshStandardMaterial({
+    map: bushColorTexture,
+    aoMap: bushARMTexture,
+    roughnessMap: bushARMTexture,
+    metalnessMap: bushARMTexture,
+    normalMap: bushNormalTexture
+})
+const bush1 = new THREE.Mesh(bushGeometry, bushMaterial)
+
+bush1.scale.set(0.5,0.5,0.5) // or setScalar if the same
+bush1.position.set(.8,0.2,2.2)
+bush1.rotation.x = -0.75
+const bush2 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush2.scale.setScalar(0.25)
+bush2.position.set(1.4, 0.2, 2.2)
+bush2.rotation.x = -0.75
+
+const bush3 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush3.scale.setScalar(0.4)
+bush3.position.set(-.8, 0.1, 2.2)
+bush3.rotation.x = -0.75
+const bush4 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush4.scale.setScalar(0.15)
+bush4.position.set(-1, 0.05, 2.6)
+bush4.rotation.x = -0.75
+house.add(bush1, bush2, bush3, bush4)
+
+
+//Graves
+const graveGeometry = new THREE.BoxGeometry(0.6, 0.8, 0.2)
+const graveMaterial = new THREE.MeshStandardMaterial()
+
+const graves = new THREE.Group()
+scene.add(graves)
+
+for (let i = 0; i < 30; i++){
+    const grave = new THREE.Mesh(graveGeometry, graveMaterial);
+
+    //add to graves
+    graves.add(grave)
+
+    const angle = Math.random() * Math.PI * 2;
+    const radius = Math.random() * 5 + 3
+    const x = radius*Math.sin(angle)
+    const z = radius* Math.cos(angle)
+    grave.rotation.x = (Math.random() - 0.5) * .4
+    grave.rotation.y = (Math.random() -.5) * .4
+    grave.rotation.z = (Math.random() -.5) * .4
+
+
+
+    grave.position.x = x
+    grave.position.z = z
+    grave.position.y = Math.random() * 0.4 + .2
+
+}
+/**
+ * Lights
+ */
+// Ambient light
+const ambientLight = new THREE.AmbientLight('#ffffff', 0.5)
+scene.add(ambientLight)
+
+// Directional light
+const directionalLight = new THREE.DirectionalLight('#ffffff', 1.5)
+directionalLight.position.set(3, 2, -8)
+scene.add(directionalLight)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 4
+camera.position.y = 2
+camera.position.z = 5
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const timer = new Timer()
+
+const tick = () =>
+{
+    // Timer
+    timer.update()
+    const elapsedTime = timer.getElapsed()
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
